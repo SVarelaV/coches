0b4c335bf968dda7fbd5a70e0a09a3191c1fae23
soniavarela <svarelavila@gmail.com>
2025-04-25 19:23:16 +0200

    parking

--- menu_grafico.py
+++ menu_grafico.py
@@ -6,15 +6,8 @@ from coche import Coche
 # Crear la ventana principal
 ventana = Tk()
 ventana.title("Gestión de Coches")
-ventana.geometry("500x300")
-ventana.resizable(False, False)
+ventana.geometry("400x300")
 ventana.config(bg="pink1")
-
-# Cargar el icono de los botones
-icono_agregar = PhotoImage(file="icono_agregar.png").subsample(20, 20)
-icono_mostrar = PhotoImage(file="icono_mostrar.png").subsample(20, 20)
-icono_eliminar = PhotoImage(file="icono_eliminar.png").subsample(20, 20)
-icono_buscar = PhotoImage(file="icono_buscar.png").subsample(20, 20)
 
 # Crear una instancia de la clase Coches
 gestor_coches = Coches()
@@ -38,19 +31,15 @@ def agregar_coche():
         entrada_modelo.delete(0, END)
         entrada_color.delete(0, END)
 
-# Función para mostrar coches
+# Función para mostrar lista de coches
 def mostrar_coches():
     lista_coches = gestor_coches.lista_coches
     if not lista_coches:
-        messagebox.showinfo("Información", "No hay coches registrados.")
+        messagebox.showinfo("Lista de Coches", "No hay coches registrados.")
     else:
-        ventana_mostrar = Toplevel(ventana)  # Nueva ventana
-        ventana_mostrar.title("Lista de Coches")
-        text_area = Text(ventana_mostrar, wrap=WORD, width=40, height=10, font=("Courier New", 10))
-        text_area.pack(padx=10, pady=10)
+        print("Lista de Coches:")
         for coche in lista_coches:
-            text_area.insert(END, f"Matrícula: {coche.matricula}, Marca: {coche.marca}, Modelo: {coche.modelo}, Color: {coche.color}\n")
-        text_area.configure(state='disabled') # Hace el Text de solo lectura
+            print(f"Matrícula: {coche.matricula}, Marca: {coche.marca}, Modelo: {coche.modelo}, Color: {coche.color}")
 
 # Función para eliminar un coche con confirmación
 def eliminar_coche():
@@ -86,40 +75,65 @@ def buscar_coche():
         messagebox.showerror("Error", f"No se encontró un coche con matrícula {matricula}.")
 
 
-# Botones y etiquetas
-boton_agregar = Button(ventana, image=icono_agregar, command=agregar_coche, borderwidth=5)
-boton_agregar.grid(row=0, column=0, pady=(10, 0), padx=15)
-Label(ventana, text="Agregar Coche", font=("Arial", 10), bg="pink1").grid(row=1, column=0, pady=(10, 0))
-
-boton_mostrar = Button(ventana, image=icono_mostrar, command=mostrar_coches, borderwidth=5)
-boton_mostrar.grid(row=0, column=1, pady=(10, 0), padx=15)
-Label(ventana, text="Mostrar Coches", font=("Arial", 10), bg="pink1").grid(row=1, column=1, pady=(10, 0))
-
-boton_eliminar = Button(ventana, image=icono_eliminar, command=eliminar_coche, borderwidth=5)
-boton_eliminar.grid(row=0, column=2, pady=(10, 0), padx=15)
-Label(ventana, text="Eliminar Coche", font=("Arial", 10), bg="pink1").grid(row=1, column=2, pady=(10, 0))
-
-boton_buscar = Button(ventana, image=icono_buscar, command=buscar_coche, borderwidth=5)
-boton_buscar.grid(row=0, column=3, pady=(10, 0), padx=15)
-Label(ventana, text="Buscar Coche", font=("Arial", 10), bg="pink1").grid(row=1, column=3, pady=(10, 0))
+# Cargar el icono de los botones
+icono_agregar = PhotoImage(file="iconos/icono_agregar.png").subsample(20, 20)
+icono_mostrar = PhotoImage(file="iconos/icono_mostrar.png").subsample(20, 20)
+icono_eliminar = PhotoImage(file="iconos/icono_eliminar.png").subsample(20, 20)
+icono_buscar = PhotoImage(file="iconos/icono_buscar.png").subsample(20, 20)
 
 
-# Etiquetas y campos de entrada
-Label(ventana, text="Matrícula:", font=("Arial", 12), bg="pink1").grid(row=2, column=0, padx=10, pady=5, sticky="e")
-entrada_matricula = Entry(ventana, font=("Arial", 12))
-entrada_matricula.grid(row=2, column=1, padx=10, pady=5)
+# Frame para agrupar los botones
+frame_botones = Frame(ventana, bg="pink1")
+frame_botones.grid(row=0, column=0, columnspan=4, pady=(10, 20))
 
-Label(ventana, text="Marca:", font=("Arial", 12), bg="pink1").grid(row=3, column=0, padx=10, pady=5, sticky="e")
-entrada_marca = Entry(ventana, font=("Arial", 12))
-entrada_marca.grid(row=3, column=1, padx=10, pady=5)
+# Botón Agregar
+frame_agregar = Frame(frame_botones, bg="pink1")
+Label(frame_agregar, text="Agregar Coche", font=("Arial", 10), bg="pink1").pack()
+boton_agregar = Button(frame_agregar, image=icono_agregar, command=agregar_coche, borderwidth=1)
+boton_agregar.pack()
+frame_agregar.pack(side=LEFT, expand=True, padx=10)
 
-Label(ventana, text="Modelo:", font=("Arial", 12), bg="pink1").grid(row=4, column=0, padx=10, pady=5, sticky="e")
-entrada_modelo = Entry(ventana, font=("Arial", 12))
-entrada_modelo.grid(row=4, column=1, padx=10, pady=5)
+# Botón Mostrar
+frame_mostrar = Frame(frame_botones, bg="pink1")
+Label(frame_mostrar, text="Mostrar Coches", font=("Arial", 10), bg="pink1").pack()
+boton_mostrar = Button(frame_mostrar, image=icono_mostrar, command=mostrar_coches, borderwidth=1)
+boton_mostrar.pack()
+frame_mostrar.pack(side=LEFT, expand=True, padx=10)
 
-Label(ventana, text="Color:", font=("Arial", 12), bg="pink1").grid(row=5, column=0, padx=10, pady=5, sticky="e")
-entrada_color = Entry(ventana, font=("Arial", 12))
-entrada_color.grid(row=5, column=1, padx=10, pady=5)
+# Botón Eliminar
+frame_eliminar = Frame(frame_botones, bg="pink1")
+Label(frame_eliminar, text="Eliminar Coche", font=("Arial", 10), bg="pink1").pack()
+boton_eliminar = Button(frame_eliminar, image=icono_eliminar, command=eliminar_coche, borderwidth=1)
+boton_eliminar.pack()
+frame_eliminar.pack(side=LEFT, expand=True, padx=10) 
+
+# Botón Buscar
+frame_buscar = Frame(frame_botones, bg="pink1")
+Label(frame_buscar, text="Buscar Coche", font=("Arial", 10), bg="pink1").pack()
+boton_buscar = Button(frame_buscar, image=icono_buscar, command=buscar_coche, borderwidth=1)
+boton_buscar.pack()
+frame_buscar.pack(side=LEFT, expand=True, padx=10)
+
+# Frame para agrupar las etiquetas y campos de entrada
+frame_campos = Frame(ventana, bg="pink1")
+frame_campos.grid(row=1, column=0, columnspan=4, pady=(10, 0))
+
+# Etiquetas y campos de entrada dentro del nuevo Frame
+Label(frame_campos, text="Matrícula:", font=("Arial", 15), bg="pink1").grid(row=0, column=0, padx=10, pady=5, sticky="e")
+entrada_matricula = Entry(frame_campos, font=("Arial", 15))
+entrada_matricula.grid(row=0, column=1, padx=10, pady=5)
+
+Label(frame_campos, text="Marca:", font=("Arial", 15), bg="pink1").grid(row=1, column=0, padx=10, pady=5, sticky="e")
+entrada_marca = Entry(frame_campos, font=("Arial", 15))
+entrada_marca.grid(row=1, column=1, padx=10, pady=5)
+
+Label(frame_campos, text="Modelo:", font=("Arial", 15), bg="pink1").grid(row=2, column=0, padx=10, pady=5, sticky="e")
+entrada_modelo = Entry(frame_campos, font=("Arial", 15))
+entrada_modelo.grid(row=2, column=1, padx=10, pady=5)
+
+Label(frame_campos, text="Color:", font=("Arial", 15), bg="pink1").grid(row=3, column=0, padx=10, pady=5, sticky="e")
+entrada_color = Entry(frame_campos, font=("Arial", 15))
+entrada_color.grid(row=3, column=1, padx=10, pady=5)
 
 
 # Iniciar el bucle principal de la ventana
